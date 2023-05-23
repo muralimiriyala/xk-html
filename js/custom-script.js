ffc0d898da29f18212d918979b0d58805974f5a7
MuraliJbk <murali@jbkinfotech.com>
2023-05-23 13:41:22 +0530

    09 page banner is done desktop line added

--- js/custom-script.js
+++ js/custom-script.js
@@ -3,8 +3,25 @@ jQuery(window).on("load scroll", function(){
     let scroll = jQuery(this).scrollTop();
     scroll > 4 ? jQuery(".main-header").addClass("fixed-header") :  jQuery(".main-header").removeClass("fixed-header");
 });
-
-
+jQuery(window).on("load resize orientationchange", function() {
+    if(jQuery(window).width() >= 768){
+        jQuery(".leadership-list").each(function() {
+            let $this = $(this).children('.leadership-text');
+            let descItem = $this.find(".leadership-desc");
+            $this.hover(function() {
+                descItem.slideDown(1000);
+            }, function() {
+                descItem.slideUp(1000);
+            });
+        });
+    }
+    else{
+        jQuery(".leadership-text").on("click", function() {
+            jQuery(this).parents().siblings().find(".leadership-desc").slideUp(1000);
+            jQuery(this).children(".leadership-desc").slideToggle(1000);
+        });
+    }
+});
 
 
 
