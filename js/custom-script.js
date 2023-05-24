a0d0f9f2c7f13701674d0370fe9f720f80204c39
MuraliJbk <murali@jbkinfotech.com>
2023-05-24 16:04:15 +0530

    from code html wrong from

--- js/custom-script.js
+++ js/custom-script.js
@@ -33,6 +33,38 @@ jQuery(document).ready(function(){
         jQuery(".financial-row").removeClass("current-q").hide();
         jQuery('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
     });
+
+
+    jQuery('ul.partners-links li:first a').addClass("active");
+    jQuery(".partners-rows-list:first").addClass("active");
+    jQuery('ul.partners-links li a').on('click', function(e){
+        e.preventDefault();
+        jQuery(this).parent().siblings().find('a').removeClass('active');
+        jQuery(this).addClass('active');
+        var attrName = jQuery(this).attr("data-value");
+        jQuery(".partners-rows-list").removeClass("active").hide();
+        jQuery('.partners-rows-list[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+
+    jQuery(".partners-btn").on("click", function(e){
+        e.preventDefault();
+        jQuery(this).toggleClass("active");
+        jQuery("ul.partners-links").slideToggle("slow");
+    });
+    jQuery(window).on("load resize orientationchange", function() {
+        if(jQuery(window).width() <= 767){
+            jQuery('ul.partners-links li a').on('click', function(e){
+                e.preventDefault();
+                jQuery(".partners-btn").removeClass("active");
+                jQuery("ul.partners-links").slideUp();
+                let ptext = jQuery(this).text();
+                let pimage = jQuery(this).children(".partners-icon").html();
+                jQuery(".partners-btn span").text(ptext);
+                jQuery(".partners-btn-icon").html(pimage);
+            });
+        }
+    });
+
 });
 
 
