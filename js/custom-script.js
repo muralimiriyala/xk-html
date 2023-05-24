2f1cf5df02e636e83a4ad064b662d11d4ba0c3c4
MuraliJbk <murali@jbkinfotech.com>
2023-05-24 20:18:45 +0530

    carees page code review started

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,12 +1,12 @@
 
-jQuery(window).on("load scroll", function(){
-    let scroll = jQuery(this).scrollTop();
-    scroll > 4 ? jQuery(".main-header").addClass("fixed-header") : jQuery(".main-header").removeClass("fixed-header");
+$(window).on("load scroll", function(){
+    let scroll = $(this).scrollTop();
+    scroll > 4 ? $(".main-header").addClass("fixed-header") : $(".main-header").removeClass("fixed-header");
 });
-jQuery(window).on("load resize orientationchange", function() {
-    if(jQuery(window).width() >= 768){
-        jQuery(".leadership-list").each(function() {
-            let $this = jQuery(this).children('.leadership-text');
+$(window).on("load resize orientationchange", function() {
+    if($(window).width() >= 768){
+        $(".leadership-list").each(function() {
+            let $this = $(this).children('.leadership-text');
             let descItem = $this.find(".leadership-desc");
             $this.hover(function() {
                 descItem.slideDown(1000);
@@ -16,55 +16,82 @@ jQuery(window).on("load resize orientationchange", function() {
         });
     }
     else{
-        jQuery(".leadership-text").on("click", function() {
-            jQuery(this).parents().siblings().find(".leadership-desc").slideUp(1000);
-            jQuery(this).children(".leadership-desc").slideToggle(1000);
+        $(".leadership-text").on("click", function() {
+            $(this).parents().siblings().find(".leadership-desc").slideUp(1000);
+            $(this).children(".leadership-desc").slideToggle(1000);
         });
     }
 });
-jQuery(document).ready(function(){
-    jQuery('ul.financial-qlinks li:first a').addClass("active");
-    jQuery(".financial-row:first").addClass("current-q");
-    jQuery('ul.financial-qlinks li a').on('click', function(e){
-        e.preventDefault();
-        jQuery(this).parent().siblings().find('a').removeClass('active');
-        jQuery(this).addClass('active');
-        var attrName = jQuery(this).attr("data-name");
-        jQuery(".financial-row").removeClass("current-q").hide();
-        jQuery('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
-    });
+$(document).ready(function(){
 
-
-    jQuery('ul.partners-links li:first a').addClass("active");
-    jQuery(".partners-rows-list:first").addClass("active");
-    jQuery('ul.partners-links li a').on('click', function(e){
+    $('ul.partners-links li:first a').addClass("active");
+    $(".partners-rows-list:first").addClass("active");
+    $('ul.partners-links li a').on('click', function(e){
         e.preventDefault();
-        jQuery(this).parent().siblings().find('a').removeClass('active');
-        jQuery(this).addClass('active');
-        var attrName = jQuery(this).attr("data-value");
-        jQuery(".partners-rows-list").removeClass("active").hide();
-        jQuery('.partners-rows-list[data-target="' + attrName + '"]').fadeIn(1000);
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-value");
+        $(".partners-rows-list").removeClass("active").hide();
+        $('.partners-rows-list[data-target="' + attrName + '"]').fadeIn(1000);
     });
-
-    jQuery(".partners-btn").on("click", function(e){
+    $(".partners-btn").on("click", function(e){
         e.preventDefault();
-        jQuery(this).toggleClass("active");
-        jQuery("ul.partners-links").slideToggle("slow");
+        $(this).toggleClass("active");
+        $("ul.partners-links").slideToggle("slow");
     });
-    jQuery(window).on("load resize orientationchange", function() {
-        if(jQuery(window).width() <= 767){
-            jQuery('ul.partners-links li a').on('click', function(e){
+    $(window).on("load resize orientationchange", function() {
+        if($(window).width() <= 767){
+            $('ul.partners-links li a').on('click', function(e){
                 e.preventDefault();
-                jQuery(".partners-btn").removeClass("active");
-                jQuery("ul.partners-links").slideUp();
-                let ptext = jQuery(this).text();
-                let pimage = jQuery(this).children(".partners-icon").html();
-                jQuery(".partners-btn span").text(ptext);
-                jQuery(".partners-btn-icon").html(pimage);
+                $(".partners-btn").removeClass("active");
+                $("ul.partners-links").slideUp();
+                let ptext = $(this).text();
+                let pimage = $(this).children(".partners-icon").html();
+                $(".partners-btn span").text(ptext);
+                $(".partners-btn-icon").html(pimage);
             });
         }
     });
 
+    $('ul.financial-qlinks li:first a').addClass("active");
+    $(".financial-row:first").addClass("current-q");
+    $('ul.financial-qlinks li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-name");
+        $(".financial-row").removeClass("current-q").hide();
+        $('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+
+    $('ul.positions-links li:first a').addClass("active");
+    $(".positions-rows-list:first").addClass("current-q");
+    $('ul.positions-links li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-value");
+        $(".financial-row").removeClass("current-q").hide();
+        $('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+    $(".positions-btn").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $("ul.positions-links").slideToggle("slow");
+    });
+    $(window).on("load resize orientationchange", function() {
+        if($(window).width() <= 767){
+            $('ul.positions-links li a').on('click', function(e){
+                e.preventDefault();
+                $(".positions-btn").removeClass("active");
+                $("ul.positions-links").slideUp();
+                let ptext = $(this).text();
+                let pimage = $(this).children(".positions-icon").html();
+                $(".positions-btn span").text(ptext);
+                $(".positions-btn-icon").html(pimage);
+            });
+        }
+    });
 });
 
 
