e73536c0627898f17495fdedf2e6e6813345c8a2
MuraliJbk <murali@jbkinfotech.com>
2023-05-25 17:36:52 +0530

    carees page code review started

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,13 @@ module.exports = function(grunt) {
       uglify: {
         my_target: {
           files: {
-            'dist/core.bundle.js': ['js/slick.min.js', 'js/custom-slick.js', 'js/jquery.selectBox.js', 'js/custom-selectBox.js', 'js/custom-script.js', 'js/magnific-popup.min.js', 'js/custom-magnific-popup.js', 'js/animation-script.js']
+            'dist/core.bundle.js': ['js/slick.min.js', 'js/custom-slick.js', 'js/jquery.selectBox.js', 'js/custom-selectBox.js', 'js/custom-script.js', 'js/magnific-popup.min.js', 'js/custom-magnific-popup.js', 'js/custom-tabs.js']
           }
         }
       },
       concat: {
         dist: {
-          src: ['js/slick.min.js', 'js/custom-slick.js', 'js/jquery.selectBox.js', 'js/custom-selectBox.js', 'js/custom-script.js', 'js/magnific-popup.min.js', 'js/custom-magnific-popup.js', 'js/animation-script.js'],
+          src: ['js/slick.min.js', 'js/custom-slick.js', 'js/jquery.selectBox.js', 'js/custom-selectBox.js', 'js/custom-script.js', 'js/magnific-popup.min.js', 'js/custom-magnific-popup.js', 'js/custom-tabs.js'],
           dest: 'dist/core.bundle.js'
         }
       },
---
+++ js/custom-tabs.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function(){
+    $('ul.partners-links li:first a').addClass("active");
+    $(".partners-rows-list:first").addClass("active");
+    $('ul.partners-links li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-value");
+        $(".partners-rows-list").removeClass("active").hide();
+        $('.partners-rows-list[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+    $(".partners-btn").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $("ul.partners-links").slideToggle("slow");
+    });
+    $(window).on("load resize orientationchange", function() {
+        if($(window).width() <= 767){
+            $('ul.partners-links li a').on('click', function(e){
+                e.preventDefault();
+                $(".partners-btn").removeClass("active");
+                $("ul.partners-links").slideUp();
+                let ptext = $(this).text();
+                let pimage = $(this).children(".partners-icon").html();
+                $(".partners-btn span").text(ptext);
+                $(".partners-btn-icon").html(pimage);
+            });
+        }
+    });
+
+    $('ul.financial-qlinks li:first a').addClass("active");
+    $(".financial-row:first").addClass("current-q");
+    $('ul.financial-qlinks li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-name");
+        $(".financial-row").removeClass("current-q").hide();
+        $('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+
+    $('ul.positions-links li:first a').addClass("active");
+    $(".positions-rows-list:first").addClass("current-q");
+    $('ul.positions-links li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active');
+        var attrName = $(this).attr("data-value");
+        $(".financial-row").removeClass("current-q").hide();
+        $('.financial-row[data-target="' + attrName + '"]').fadeIn(1000);
+    });
+    $(".positions-btn").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $("ul.positions-links").slideToggle("slow");
+    });
+    $(window).on("load resize orientationchange", function() {
+        if($(window).width() <= 767){
+            $('ul.positions-links li a').on('click', function(e){
+                e.preventDefault();
+                $(".positions-btn").removeClass("active");
+                $("ul.positions-links").slideUp();
+                let ptext = $(this).text();
+                let pimage = $(this).children(".positions-icon").html();
+                $(".positions-btn span").text(ptext);
+                $(".positions-btn-icon").html(pimage);
+            });
+        }
+    });
+});
