e272a77536bab8e8080e911d2a1b64e8458b44e2
MuraliJbk <murali@jbkinfotech.com>
2023-05-27 19:22:44 +0530

    text link revrse added

---
+++ js/animation-script.js
@@ -0,0 +1,26 @@
+var $ = jQuery.noConflict();
+var $animation_elements = $('[data-animation]');
+var $window = $(window);
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    let $attr = $element.attr('data-animation');
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('visible');
+      $element.addClass($attr);
+    } else {
+      //$element.removeClass('visible');
+    }
+  });
+}
+$window.on('scroll load', check_if_in_view);
+$window.trigger('scroll load');
